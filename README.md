e612af790c70f8566844b4e25f1b870be5bed46e
Anthony Nixon <ajnixon@spscommerce.com>
2019-02-01 13:12:16 -0600

    Add v0.0.2

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # pseudo-build
+Docker image to simulate a build in CI systems (like drone).
+
+## Environment Variables
+- `OUTCOME`: Either `SUCCESS` or `FAILURE`. Determines if the mock build succeeds or fails. Defaults to `SUCCESS` if not set.
+
+## Using
+This image is [available in dockerhub](https://cloud.docker.com/u/anthonynixon/repository/docker/anthonynixon/pseudo-build)
+
+`anthonynixon/pseudo-build`
--- script.sh
+++ script.sh
@@ -1,5 +1,15 @@
+if [[ $OUTCOME == "" ]]; then
+  OUTCOME=SUCCESS
+fi
+
 echo "Build starting..."
 SLEEPTIME=$((1 + RANDOM % 10))
-echo "This build should complete in ($SLEEPTIME) seconds..."
+echo "This build should complete with $OUTCOME status in ($SLEEPTIME) seconds..."
 sleep $SLEEPTIME
-echo "Build complete."
+if [ $OUTCOME = "SUCCESS" ]; then
+  echo "Build complete."
+  exit 0
+else
+  echo "Build failed."
+  exit 1
+fi
